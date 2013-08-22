389c744aaa056602b371c13665ee2619b58e3f37
Fantomas42 <fantomas42@gmail.com>
2013-08-22 21:01:09 +0200

    First clean-up of the write method

--- bvc/__init__.py
+++ bvc/__init__.py
@@ -17,22 +17,20 @@ class VersionsConfigParser(RawConfigParser):
     optionxform = str
 
     def write(self, fp):
-        """Write an .ini-format representation of the configuration state."""
-        if self._defaults:
-            fp.write("[%s]\n" % DEFAULTSECT)
-            for (key, value) in self._defaults.items():
-                fp.write("%s = %s\n" % (key, str(value).replace('\n', '\n\t')))
-            fp.write("\n")
+        """
+        Write an .ini-format representation of the
+        configuration state with readable indentation.
+        """
         for section in self._sections:
-            fp.write("[%s]\n" % section)
+            fp.write('[%s]\n' % section)
             for (key, value) in self._sections[section].items():
-                if key != "__name__":
+                if key != '__name__':
                     if value is None:
-                        fp.write("%s\n" % (key))
+                        fp.write('%s\n' % (key))
                     else:
-                        fp.write("%s = %s\n" %
+                        fp.write('%s = %s\n' %
                                  (key, str(value).replace('\n', '\n\t')))
-            fp.write("\n")
+            fp.write('\n')
 
 
 class VersionsChecker(object):
