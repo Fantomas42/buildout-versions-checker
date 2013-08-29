85206ac334e8826463e67e3cf55491dc88415747
Fantomas42 <fantomas42@gmail.com>
2013-08-29 20:39:16 +0200

    Preparing tests

---
+++ bvc/tests.py
@@ -0,0 +1,17 @@
+"""Tests for Buildout version checker"""
+from unittest import TestCase
+
+from bvc import VersionsChecker
+from bvc import VersionsConfigParser
+
+
+class VersionsCheckerTestCase(TestCase):
+    pass
+
+
+class VersionsConfigParserTestCase(TestCase):
+    pass
+
+
+class CommandLineTestCase(TestCase):
+    pass
