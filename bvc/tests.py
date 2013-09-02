8d5c347b942e5f787fed3dbb0e9fb4c8cff810ef
Fantomas42 <fantomas42@gmail.com>
2013-09-02 14:53:34 +0200

    Test parsing with case insensitive

--- bvc/tests.py
+++ bvc/tests.py
@@ -1,4 +1,6 @@
 """Tests for Buildout version checker"""
+from tempfile import TemporaryFile
+
 from unittest import TestCase
 from unittest import TestSuite
 from unittest import TestLoader
@@ -15,7 +17,16 @@ class VersionsCheckerTestCase(TestCase):
 
 
 class VersionsConfigParserTestCase(TestCase):
-    pass
+
+    def test_parse_case_insensitive(self):
+        config_file = TemporaryFile()
+        config_file.write('[Section]\nKEY=VALUE\nKey=Value\n')
+        config_file.seek(0)
+        config_parser = VersionsConfigParser()
+        config_parser.readfp(config_file)
+        self.assertEquals(config_parser.sections(), ['Section'])
+        self.assertEquals(config_parser.options('Section'), ['KEY', 'Key'])
+        config_file.close()
 
 
 class CommandLineTestCase(TestCase):
