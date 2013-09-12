7ae8f25fc33e804ed8a64c7eb4d535d1962bbf86
Fantomas42 <fantomas42@gmail.com>
2013-09-12 20:21:45 +0200

    Fix error in classifiers

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     keywords='buildout, versions, updates',
     classifiers=[
         'Framework :: Buildout',
-        'Intended Audience :: Developers'
+        'Intended Audience :: Developers',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
