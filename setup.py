7bed195ba63ac3d9b3da2045c3e6d08d5f7b94b1
Fantomas42 <fantomas42@gmail.com>
2013-10-02 14:03:03 +0200

    Remove constraint on versions required

--- setup.py
+++ setup.py
@@ -39,8 +39,8 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Topic :: Software Development :: Build Tools',
         'Topic :: Software Development :: Libraries :: Python Modules'],
-    install_requires=['six>=1.4.1',
-                      'futures>=2.1.4'],
+    install_requires=['six',
+                      'futures'],
     entry_points={
         'console_scripts': 'check-buildout-updates=bvc:cmdline'},
     test_suite='bvc.tests.test_suite',
