4d00e04ec48f8acd88b7d4b786da343f203ea7d0
Fantomas42 <fantomas42@gmail.com>
2014-02-14 20:54:18 +0100

    Make the alpha sorting working

--- bvc/configparser.py
+++ bvc/configparser.py
@@ -15,13 +15,23 @@ class VersionsConfigParser(RawConfigParser):
     """
     optionxform = str
 
-    def write_section(self, fd, section, indentation):
+    def alpha_sorter(self, items):
+        return sorted(items, key=lambda x: x[0])
+
+    def write_section(self, fd, section, indentation, sorting):
         """
         Write a section of an .ini-format
         and all the keys within.
         """
         string_section = '[%s]\n' % section
-        for key, value in self._sections[section].items():
+
+        items = self._sections[section].items()
+        try:
+            items = getattr(self, '%s_sorter' % sorting)(items)
+        except (TypeError, AttributeError):
+            pass
+
+        for key, value in items:
             if key == '__name__':
                 continue
             if value is None:
@@ -44,7 +54,7 @@ class VersionsConfigParser(RawConfigParser):
 
         fd.write(string_section.encode('utf-8'))
 
-    def write(self, source, indentation=32):
+    def write(self, source, indentation=32, sorting=None):
         """
         Write an .ini-format representation of the
         configuration state with a readable indentation.
@@ -52,6 +62,6 @@ class VersionsConfigParser(RawConfigParser):
         with open(source, 'wb') as fd:
             sections = list(self._sections.keys())
             for section in sections[:-1]:
-                self.write_section(fd, section, indentation)
+                self.write_section(fd, section, indentation, sorting)
                 fd.write('\n'.encode('utf-8'))
-            self.write_section(fd, sections[-1], indentation)
+            self.write_section(fd, sections[-1], indentation, sorting)
